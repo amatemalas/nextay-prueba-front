aff5ba4e15c0311f94058494a8454e8df4c9dccc
Adrián Matemalas <amatemalas@gmail.com>
2026-08-18 13:52:58 +0200

    Fix name miss spell

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" />
 
-        <q-toolbar-title> Nextray </q-toolbar-title>
+        <q-toolbar-title> Nextay </q-toolbar-title>
         <div>Prueba técnica</div>
       </q-toolbar>
     </q-header>
